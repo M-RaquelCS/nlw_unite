9dfd1a379da4bab61174f92cb2c50ee1cf2ebb85
Rachel <M-Raquel@outlook.com.br>
2024-04-02 09:05:39 -0300

    class one

--- reactjs/pass-in-web/src/App.tsx
+++ reactjs/pass-in-web/src/App.tsx
@@ -1,7 +1,11 @@
+import { AttendeeList } from "./components/Attendee-list";
+import { Header } from "./components/Header";
+
 export function App() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5">
+      <Header />
+      <AttendeeList />
+    </div>
   )
 }
